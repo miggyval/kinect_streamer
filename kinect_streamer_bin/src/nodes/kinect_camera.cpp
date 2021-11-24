a83d17d5a1e6057b63441414067fee649965b2ab
Miguel Marco Valencia <valencimm@gmail.com>
2021-11-24 17:05:26 +1000

    Kinect Camera Calibration Added

--- kinect_streamer_bin/src/nodes/kinect_camera.cpp
+++ kinect_streamer_bin/src/nodes/kinect_camera.cpp
@@ -8,6 +8,7 @@
 #include <libfreenect2/logger.h>
 
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 
@@ -15,10 +16,15 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
+#include <camera_info_manager/camera_info_manager.h>
+
 int main(int argc, char** argv) {
+    libfreenect2::setGlobalLogger(NULL);
     std::string serial_a = "501530742442";
     std::string serial_b = "226287140347";
     ros::init(argc, argv, "kinect_camera");
+    bool show = false;
+    bool info = true;
     try {
         KinectStreamer::KinectDevice kin_dev_a(serial_a);
         KinectStreamer::KinectDevice kin_dev_b(serial_b);
@@ -30,14 +36,25 @@ int main(int argc, char** argv) {
             std::cout << "Failed to start Kinect B." << std::endl;
             exit(-1);
         }
+        //
+            ros::NodeHandle nh_camera_color;
+            ros::NodeHandle nh_camera_color_a(nh_camera_color, "color_a");
+            ros::NodeHandle nh_camera_color_b(nh_camera_color, "color_b");
+            camera_info_manager::CameraInfoManager manager_color_a(nh_camera_color_a, "color_a");
+            camera_info_manager::CameraInfoManager manager_color_b(nh_camera_color_b, "color_b");
+            manager_color_a.loadCameraInfo("");
+            manager_color_b.loadCameraInfo("");
+        //
         ros::NodeHandle nh_color_a;
         ros::NodeHandle nh_color_b;
         image_transport::ImageTransport it_color_a(nh_color_a);
         image_transport::ImageTransport it_color_b(nh_color_b);
-        image_transport::Publisher pub_color_a = it_color_a.advertise("color_a/image_raw", 10);
-        image_transport::Publisher pub_color_b = it_color_b.advertise("color_b/image_raw", 10);
-        cv::namedWindow("Color A", cv::WINDOW_NORMAL);
-        cv::namedWindow("Color B", cv::WINDOW_NORMAL);
+        image_transport::Publisher pub_color_a = it_color_a.advertise("color_a/image_raw", 1);
+        image_transport::Publisher pub_color_b = it_color_b.advertise("color_b/image_raw", 1);
+        if (show) {
+            cv::namedWindow("Color A", cv::WINDOW_NORMAL);
+            cv::namedWindow("Color B", cv::WINDOW_NORMAL);
+        }
         while (ros::ok()) {
             libfreenect2::Frame* color_a = kin_dev_a.get_frame(libfreenect2::Frame::Color);
             libfreenect2::Frame* color_b = kin_dev_b.get_frame(libfreenect2::Frame::Color);
@@ -49,13 +66,20 @@ int main(int argc, char** argv) {
             sensor_msgs::ImagePtr msg_color_b = cv_bridge::CvImage(std_msgs::Header(), "bgra8", img_color_b).toImageMsg();
             pub_color_a.publish(msg_color_a);
             pub_color_b.publish(msg_color_b);
-            cv::imshow("Color A", img_color_a);
-            cv::imshow("Color B", img_color_b);
+            if (show) {
+                cv::imshow("Color A", img_color_a);
+                cv::imshow("Color B", img_color_b);
+                cv::resizeWindow("Color A", cv::Size(960, 540));
+                cv::resizeWindow("Color B", cv::Size(960, 540));
+                char c = cv::waitKey(1);
+                if (c == ' ') {
+                    while (cv::waitKey(1) != ' ') {
+                        
+                    }   
+                }
+            }
             kin_dev_a.release_frames();
             kin_dev_b.release_frames();
-            cv::resizeWindow("Color A", cv::Size(960, 540));
-            cv::resizeWindow("Color B", cv::Size(960, 540));
-            cv::waitKey(1);
             
         }
     } catch (std::exception e) {
